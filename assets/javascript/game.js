58ae1d386a0a8682cc1bc900eea55f39135d693f
notintheup <brianjandron@gmail.com>
2019-02-10 14:53:29 -0500

    finally have my addition working on click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,41 @@
 // Global space
 $(document).ready(function () {
+
   //Declare variables for scoreboard
   var current = 0;
   var wins = 0;
   var losses = 0;
+  //Generates a random number
   var target = Math.floor(Math.random() * 101 + 19)
-
+  var crystal1 = (Math.floor(Math.random() * 10 + 2));
+  var crystal2 = (Math.floor(Math.random() * 11 + 2));
+  var crystal3 = (Math.floor(Math.random() * 12 + 2));
+  var crystal4 = (Math.floor(Math.random() * 13 + 2));
+  console.log(crystal1); 
+  console.log(crystal2); 
+  console.log(crystal3); 
+  console.log(crystal4); 
   $("h5.card-wins").html("Total wins:  " + wins);
   $("h5.card-losses").html("Total losses:  " + losses);
   $("h5.card-current").html("Current Total:  " + current);
   $("h5.card-target").html("Your target:  " + target);
 
-  //Generates a random target number
-  //Game variables
-  var crystal1 = (Math.floor(Math.random() * 10 + 2));
-  var crystal2 = (Math.floor(Math.random() * 11 + 2));
-  var crystal3 = (Math.floor(Math.random() * 12 + 2));
-  var crystal4 = (Math.floor(Math.random() * 13 + 2));
-  console.log(crystal1);
-  console.log(crystal2);
-  console.log(crystal3);
-  console.log(crystal4);
-})
-
-//Game 
-$(".gem1, .gem2, .gem3, .gem4").on({
-  mouseenter: function(){
-    $(this).css("background-color", "purple");
-  }, 
-  mouseleave: function(){
-    $(this).css("background-color", "lightblue");
-  }, 
-  click: function(){
-    clickVal += $(this).val();
-    $("h5.card-current").text("current" + clickVal);
-  } 
+  //Game runtime 
+  $("img.gem1").click(function () {
+    current = current + crystal1;
+    $("h5.card-current").html("Current Total: " + current)
+  })
+  $("img.gem2").click(function () {
+    current = current + crystal2;
+    $("h5.card-current").html("Current Total: " + current)
+  })
+  $("img.gem3").click(function () {
+    current = current + crystal3;
+    $("h5.card-current").html("Current Total: " + current)
+  })
+  $("img.gem4").click(function () {
+    current = current + crystal4;
+    $("h5.card-current").html("Current Total: " + current)
+  })
 });
 
