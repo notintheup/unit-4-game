e29d5c880fb649f2b0ea57213a9c7eeac437bd28
notintheup <brianjandron@gmail.com>
2019-02-11 09:05:34 -0500

    final cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,31 +11,70 @@ $(document).ready(function () {
   var crystal2 = (Math.floor(Math.random() * 11 + 2));
   var crystal3 = (Math.floor(Math.random() * 12 + 2));
   var crystal4 = (Math.floor(Math.random() * 13 + 2));
-  console.log(crystal1); 
-  console.log(crystal2); 
-  console.log(crystal3); 
-  console.log(crystal4); 
+
   $("h5.card-wins").html("Total wins:  " + wins);
   $("h5.card-losses").html("Total losses:  " + losses);
   $("h5.card-current").html("Current Total:  " + current);
   $("h5.card-target").html("Your target:  " + target);
 
+  function win() {
+    wins++
+    $("h5.card-wins").html("Total wins:  " + (wins));
+    reset();
+  }
+
+  function lose() {
+    losses++
+    $("h5.card-losses").html("Total losses: " + (losses));
+    reset();
+  }
+
+  function reset() {
+    current = 0;
+    target = Math.floor(Math.random() * 101 + 19);
+    $("h5.card-target").html("Your target:  " + target);
+    crystal1 = (Math.floor(Math.random() * 10 + 2));
+    crystal2 = (Math.floor(Math.random() * 11 + 2));
+    crystal3 = (Math.floor(Math.random() * 12 + 2));
+    crystal4 = (Math.floor(Math.random() * 13 + 2));
+    // $("h5.card-current").html("Current Total:  " + current);
+  }
   //Game runtime 
   $("img.gem1").click(function () {
     current = current + crystal1;
-    $("h5.card-current").html("Current Total: " + current)
+    $("h5.card-current").html("Current Total: " + current);
+    if (target === current) {
+      win();
+    } else if (current >= target) {
+      lose();
+    }
   })
   $("img.gem2").click(function () {
     current = current + crystal2;
-    $("h5.card-current").html("Current Total: " + current)
+    $("h5.card-current").html("Current Total: " + current);
+    if (target === current) {
+      win();
+    } else if (current >= target) {
+      lose();
+    }
   })
   $("img.gem3").click(function () {
     current = current + crystal3;
-    $("h5.card-current").html("Current Total: " + current)
+    $("h5.card-current").html("Current Total: " + current);
+    if (target === current) {
+      win();
+    } else if (current >= target) {
+      lose();
+    }
   })
   $("img.gem4").click(function () {
     current = current + crystal4;
-    $("h5.card-current").html("Current Total: " + current)
+    $("h5.card-current").html("Current Total: " + current);
+    if (target === current) {
+      win();
+    } else if (current >= target) {
+      lose();
+    }
   })
-});
 
+});
